7ade4ce8ec2f22ac7f02b89939349171c52f8d66
vishwasPuri-maker <purijaswant46@gmail.com>
2026-05-16 13:29:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (47.71%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int leveles(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        return 1+max(leveles(root->left),leveles(root->right));
+    }
+    void nthLevel(TreeNode* root, int curr , int level, vector<int>& v){
+        if(root==NULL){
+            return;
+        }
+        if(curr==level){
+            v.push_back(root->val);
+            // cout<<endl;
+        }
+        nthLevel(root->left,curr+1,level,v);
+        nthLevel(root->right,curr+1,level,v);
+    }
+    void lOrder(TreeNode* root,vector<vector<int>>& ans){
+        int n=leveles(root);
+        for(int i=1;i<=n;i++){
+            vector<int> v;  // Here we are pushing the value to the vector 
+            nthLevel(root,1,i,v);
+            if(i%2==0){
+                reverse(v.begin(),v.end());
+                ans.push_back(v);
+            }
+            else ans.push_back(v);   // Here we are pushing vector to the another vector which we return in answer according to question 
+            cout<<endl;
+        }
+    }
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        lOrder(root,ans);
+        return ans;
+    }
+};
