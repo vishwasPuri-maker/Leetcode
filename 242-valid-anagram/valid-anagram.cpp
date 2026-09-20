7057cb15531eb302a73daeefb920da186ac8d338
vishwasPuri-maker <purijaswant46@gmail.com>
2026-09-20 23:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (11.23%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        unordered_map<char,int> map1;
+        unordered_map<char,int> map2;
+
+        for(int i = 0 ;i<t.length() ; i++){
+            map2[t[i]]++;
+        }
+        for(int i = 0 ;i<s.length();i++){
+            map1[s[i]]++;
+        }
+
+        for(auto x : map1){
+            char ch1 = x.first;
+            int freq1 = x.second;
+
+            if(map2.find(ch1)!= map2.end()){
+                int freq2 = map2[ch1];
+                if(freq1 != freq2){
+                    return false;
+                }
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
