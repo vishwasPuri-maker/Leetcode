a27b195994899149a40aa8d6b82e701a9a3a7f77
vishwasPuri-maker <purijaswant46@gmail.com>
2026-09-26 15:56:56 +0530

    Time: 3 ms (72.23%) | Memory: 48.7 MB (42.11%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<int> ans;
-        unordered_set<int> set;
+        // vector<int> ans;
+        // unordered_set<int> set;
 
-        for(int i =0 ; i<nums.size() ; i++){
-            if(set.find(nums[i])!=set.end()){
-                ans.push_back(nums[i]);
+        // for(int i =0 ; i<nums.size() ; i++){
+        //     if(set.find(nums[i])!=set.end()){
+        //         ans.push_back(nums[i]);
+        //     }
+        //     else{
+        //         set.insert(nums[i]);
+        //     }
+        // }
+        // return ans;
+        // Yes this is the one solution to solve this question but this is taking the extra space so now were solve this with no extra space 
+
+        vector<int> ans;
+        for(int i=0; i<nums.size() ; i++){
+            int index = abs(nums[i])-1;
+            if(nums[index]<0){
+                ans.push_back(index+1);
             }
-            else{
-                set.insert(nums[i]);
-            }
+            nums[index] = nums[index]*-1;
         }
         return ans;
     }
